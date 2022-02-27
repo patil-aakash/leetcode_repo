71903b25040db6e0eca1e7f3b55c7ca98a642299
Aakash Patil <patil.aa@northeastern.edu>
2022-02-27 14:54:11 -0500

    Array permutation and maximum sub array

---
+++ FindtheDuplicateNumber_287M.java
@@ -0,0 +1,23 @@
+public class FindtheDuplicateNumber_287M {
+    public static void main(String[] args) {
+        int nums[] = {1,3,4,2,2};
+        FindtheDuplicateNumber_287M f = new FindtheDuplicateNumber_287M();
+        System.out.println(f.findDuplicate(nums));
+    }
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
